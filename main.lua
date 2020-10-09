e2a915ad12489cf2390d13c6d8b0e969fc57165a
sophia <sophiabaldonado@gmail.com>
2020-10-08 23:41:33 -0700

    preview next photo in sphere; bubble shader;

--- main.lua
+++ main.lua
@@ -1,15 +1,16 @@
 function lovr.load()
     models = {}
     photos = {
-        lovr.graphics.newTexture('country.jpg'),
-        lovr.graphics.newTexture('mountains.jpg'),
-        lovr.graphics.newTexture('windmill.jpg')
+        lovr.graphics.newMaterial(lovr.graphics.newTexture('country.jpg', { mipmaps = false })),
+        lovr.graphics.newMaterial(lovr.graphics.newTexture('mountains.jpg', { mipmaps = false })),
+        lovr.graphics.newMaterial(lovr.graphics.newTexture('windmill.jpg', { mipmaps = false }))
     }
     sphere = {
         position = lovr.math.newVec3(0, 1.2, -1),
         radius = .15
     }
     currentPhoto = 1
+    shader = lovr.graphics.newShader(bubbleShader())
 end
 
 function lovr.update(dt)
@@ -19,20 +20,29 @@ function lovr.update(dt)
         inSphere = (d.x * d.x + d.y * d.y + d.z * d.z) < math.pow(sphere.radius, 2)
         if inSphere then
             if lovr.headset.wasPressed(hand, 'trigger') then
-                swapPhoto()
+                cyclePhoto()
             end
         end
     end
 end
 
 function lovr.draw()
-    lovr.graphics.skybox(photos[currentPhoto])
+    lovr.graphics.skybox(photos[currentPhoto]:getTexture())
 
+    drawSphere()
+    drawControllers()
+end
+
+function drawSphere()
     local color = inSphere and 0xffffff or 0xd3d3d3
     lovr.graphics.setColor(color)
-    lovr.graphics.sphere(sphere.position.x, sphere.position.y, sphere.position.z, sphere.radius)
+    lovr.graphics.setShader(shader)
+    lovr.graphics.sphere(photos[nextPhoto()], sphere.position, sphere.radius)
+    lovr.graphics.setShader()
     lovr.graphics.setColor(0xffffff)
+end
 
+function drawControllers()
     for i, hand in ipairs(lovr.headset.getHands()) do
         models[hand] = models[hand] or lovr.headset.newModel(hand)
 
@@ -43,6 +53,34 @@ function lovr.draw()
     end
 end
 
-function swapPhoto()
-    currentPhoto = (currentPhoto % #photos) + 1
+function cyclePhoto()
+    currentPhoto = nextPhoto()
+end
+
+function nextPhoto()
+    return (currentPhoto % #photos) + 1
+end
+
+function bubbleShader()
+    return [[
+        out vec3 FragmentPos;
+        out vec3 CameraPos;
+        out vec3 Normal;
+        vec4 position(mat4 projection, mat4 transform, vec4 vertex) {
+            Normal = (lovrNormalMatrix * lovrNormal).xyz;
+            FragmentPos = (lovrModel * vertex).xyz;
+            CameraPos = -lovrView[3].xyz * mat3(lovrView);
+            return projection * transform * vertex;
+        }
+        ]], [[
+        in vec3 Normal;
+        in vec3 FragmentPos;
+        in vec3 CameraPos;
+        vec4 color(vec4 graphicsColor, sampler2D image, vec2 uv) {
+            vec3 toCam = normalize(CameraPos - FragmentPos);
+            float rimAmt = 1.0 - max(0.0, dot(normalize(Normal), toCam));
+            rimAmt = pow(rimAmt, 3.5);
+            return mix(texture(image, uv) * graphicsColor, vec4(1.), rimAmt);
+        }
+    ]]
 end
