81a4b0a05e3db1660ff0037221c5d8ac82685830
sophia <sophiabaldonado@gmail.com>
2020-10-13 17:00:02 -0700

    flip normals of selection sphere and add a highlight shader;

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 function lovr.load()
-    models = {}
+    controllerModels = {}
     photos = {
         lovr.graphics.newMaterial(lovr.graphics.newTexture('country.jpg', { mipmaps = false })),
         lovr.graphics.newMaterial(lovr.graphics.newTexture('mountains.jpg', { mipmaps = false })),
@@ -13,9 +13,14 @@ function lovr.load()
         posRelativeToHead = lovr.math.newVec3(),
         yOverride = 1.1
     }
+    constants = {
+        headOffset = lovr.math.newVec3(0, -0.5, -0.6),
+        grey = 0xd3d3d3,
+        white = 0xffffff
+    }
     currentPhoto = 1
-    headOffset = lovr.math.newVec3(0, -0.5, -0.6)
-    shader = lovr.graphics.newShader(bubbleShader())
+    rimLightShader = lovr.graphics.newShader(createRimLightShader())
+    highlightShader = lovr.graphics.newShader(createHighlightShader())
 end
 
 -- App cycle
@@ -40,7 +45,7 @@ function repositionSelectionSphere(variator)
     local headPos = lovr.math.vec3(lovr.headset.getPose())
     local sphereNearHead = sphereDistanceTest(headPos, sphere.translateThreshold)
     if not sphereNearHead then
-        local v = lovr.math.vec3(mat4(lovr.headset.getPose()):translate(headOffset))
+        local v = lovr.math.vec3(mat4(lovr.headset.getPose()):translate(constants.headOffset))
         sphere.posRelativeToHead:set(v.x, sphere.yOverride, v.z)
         sphere.translater = math.min(sphere.translater + variator * .05, 1)
     end
@@ -55,23 +60,37 @@ function lovr.draw()
 end
 
 function drawSphere()
-    local color = handInSphere and 0xffffff or 0xd3d3d3
+    local color = handInSphere and constants.white or constants.grey
     local ease = quadraticEaseInOut(sphere.translater)
-    lovr.graphics.setColor(color)
-    lovr.graphics.setShader(shader)
     sphere.position:lerp(sphere.posRelativeToHead, ease)
+
+    lovr.graphics.setColor(color)
+    lovr.graphics.setShader(rimLightShader)
+    lovr.graphics.setBlendMode('alpha')
+    lovr.graphics.setCullingEnabled(true)
+    lovr.graphics.setWinding('clockwise')
+
     lovr.graphics.sphere(photos[nextPhoto()], sphere.position, sphere.radius)
+    lovr.graphics.setWinding('counterclockwise')
+
+    lovr.graphics.setDepthTest(nil, false)
+    lovr.graphics.setShader(highlightShader)
+    lovr.graphics.sphere(photos[nextPhoto()], sphere.position, sphere.radius)
+
+    lovr.graphics.setDepthTest(nil, true)
+    lovr.graphics.setBlendMode()
+    lovr.graphics.setCullingEnabled(false)
     lovr.graphics.setShader()
-    lovr.graphics.setColor(0xffffff)
+    lovr.graphics.setColor(constants.white)
 end
 
 function drawControllers()
     for i, hand in ipairs(lovr.headset.getHands()) do
-        models[hand] = models[hand] or lovr.headset.newModel(hand)
+        controllerModels[hand] = controllerModels[hand] or lovr.headset.newModel(hand)
 
-        if models[hand] then
+        if controllerModels[hand] then
             local x, y, z, angle, ax, ay, az = lovr.headset.getPose(hand)
-            models[hand]:draw(x, y, z, 1, angle, ax, ay, az)
+            controllerModels[hand]:draw(x, y, z, 1, angle, ax, ay, az)
         end
     end
 end
@@ -95,13 +114,13 @@ function quadraticEaseInOut(val)
 end
 
 -- Shader
-function bubbleShader()
+function createRimLightShader()
     return [[
         out vec3 FragmentPos;
         out vec3 CameraPos;
         out vec3 Normal;
         vec4 position(mat4 projection, mat4 transform, vec4 vertex) {
-            Normal = (lovrNormalMatrix * lovrNormal).xyz;
+            Normal = (lovrNormalMatrix * -lovrNormal).xyz;
             FragmentPos = (lovrModel * vertex).xyz;
             CameraPos = -lovrView[3].xyz * mat3(lovrView);
             return projection * transform * vertex;
@@ -113,8 +132,33 @@ function bubbleShader()
         vec4 color(vec4 graphicsColor, sampler2D image, vec2 uv) {
             vec3 toCam = normalize(CameraPos - FragmentPos);
             float rimAmt = 1.0 - max(0.0, dot(normalize(Normal), toCam));
-            rimAmt = pow(rimAmt, 3.5);
-            return mix(texture(image, uv) * graphicsColor, vec4(1.), rimAmt);
+            rimAmt = pow(rimAmt, 3.);
+            return mix(texture(image, uv) * graphicsColor * .9, vec4(1.), rimAmt);
+        }
+    ]]
+end
+
+function createHighlightShader()
+    return [[
+        out vec3 FragmentPos;
+        out vec3 CameraPos;
+        out vec3 Normal;
+        vec4 position(mat4 projection, mat4 transform, vec4 vertex) {
+            Normal = (lovrNormalMatrix * -lovrNormal).xyz;
+            FragmentPos = (lovrModel * vertex).xyz;
+            CameraPos = -lovrView[3].xyz * mat3(lovrView);
+            return projection * transform * vertex;
+        }
+        ]], [[
+        in vec3 Normal;
+        in vec3 FragmentPos;
+        in vec3 CameraPos;
+        vec4 color(vec4 graphicsColor, sampler2D image, vec2 uv) {
+            vec3 lightDir = normalize(vec3(.4, 2, -.1));
+            vec3 viewDir = normalize(CameraPos - FragmentPos);
+            vec3 reflectDir = reflect(-lightDir, normalize(Normal));
+            float spec = pow(max(dot(viewDir, reflectDir), 0.0), 25.);
+            return vec4(1., 1., 1., spec * .75);
         }
     ]]
 end
